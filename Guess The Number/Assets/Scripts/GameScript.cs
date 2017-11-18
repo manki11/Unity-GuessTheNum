d8f34c5a6854c19477895f8114d19a9f00c20923
Mankirat Singh <mankirat1998@gmail.com>
2017-11-18 19:52:15 +0530

    feat: game added

---
+++ Guess The Number/Assets/Scripts/GameScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScript : MonoBehaviour {
+
+	public InputField input;
+	public Text infoText;
+
+	private int guessNumber;
+	private int usersGuess;
+
+	// Use this for initialization
+	void Start () {
+		guessNumber = Random.Range (0, 100);
+	}
+	
+	public void checkGuess(){
+		usersGuess = int.Parse(input.text);
+
+		if (guessNumber == usersGuess) {
+			infoText.text = "You guessed the number. You Won. Guess a number again!";
+			Start ();
+		} else if (usersGuess > guessNumber) {
+			infoText.text = "Your number is big !!";
+		} else {
+			infoText.text = "Your number is small !!";
+		}
+
+		input.text = "";
+	}
+}
